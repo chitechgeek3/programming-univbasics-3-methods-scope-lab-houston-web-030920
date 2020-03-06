ea58ec97cebe3de9b6666b181662661b43c834a8
Rob Lawrence <iamrobnyce@gmail.com>
2020-03-06 02:29:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,21 +1,19 @@
 def mario
-  phrase = "It's-a me, Mario!"
-  
-  puts phrase
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts "It's-a me, Mario!"
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status
+  puts "Thank You Mario! But Our Princess Is In Another Castle!"
 end
 
-def link 
+def link
   puts "It's Dangerous To Go Alone! Take This."
 end
 
-def any_phrase (phrase)
-  puts  "It's-a me, Mario!\n/",
-end 
+def any_phrase(phrase)
+  puts "Do A Barrel Roll!"
+end
 
 
 
